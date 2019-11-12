09d21286ed409b8caf92643b9d540b5cab18412f
Lucas Heim <lucasedfc@gmail.com>
2019-11-11 22:45:36 -0300

    Connect MongoDB

--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./classes/server"));
 const user_1 = __importDefault(require("./routes/user"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const server = new server_1.default;
 // Routes
 server.app.use('/user', user_1.default);
+// Connect MongoDB
+mongoose_1.default.connect('mongodb://localhost:27017/fotosgram', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err) => {
+    if (err)
+        throw err;
+    console.log('MongoDB Connected');
+});
 //Init express
 server.start(() => {
     console.log(`Server started on ${server.port}`);
--- index.ts
+++ index.ts
@@ -1,10 +1,19 @@
 import Server from "./classes/server";
 import userRoutes from "./routes/user";
+import mongoose from 'mongoose';
 
 const server = new Server;
 
 // Routes
-server.app.use('/user', userRoutes )
+server.app.use('/user', userRoutes );
+
+// Connect MongoDB
+mongoose.connect('mongodb://localhost:27017/fotosgram', 
+{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}, (err) => {
+    
+    if(err) throw err;
+    console.log('MongoDB Connected');
+    });
 
 //Init express
 server.start( () => {
