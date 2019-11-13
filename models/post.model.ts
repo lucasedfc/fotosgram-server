c83a4729bf499918a89440e30258542aa2423144
Lucas Heim <lucasedfc@gmail.com>
2019-11-12 21:39:58 -0300

    Post model

---
+++ models/post.model.ts
@@ -0,0 +1,36 @@
+import {Schema, Document, model} from 'mongoose';
+
+const postSchema = new Schema({
+    created: {
+        type: Date
+    },
+    message: {
+        type: String
+    },
+    img: [{
+        type: String
+    }],
+    coords: {
+        type: String
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'User references required']
+    }
+});
+
+postSchema.pre<IPost>('save', function( next) {
+    this.created = new Date();
+    next();
+});
+
+interface IPost extends Document {
+    created: Date;
+    message: string;
+    img: string;
+    coords: string;
+    user: string;
+}
+
+export const Post = model<IPost>('Post', postSchema);
