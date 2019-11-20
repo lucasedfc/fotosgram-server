42de945fcee7e6c8ef6247a38c645fc62d81160f
Lucas Heim <lucasedfc@gmail.com>
2019-11-19 21:17:22 -0300

    Get posts paginated

--- routes/post.ts
+++ routes/post.ts
@@ -4,7 +4,31 @@ import { Router, Response } from "express";
 
 const postRoutes = Router();
 
+// Get paginated posts
+postRoutes.get('/', async (req: any, res: Response) => {
 
+    let page = Number(req.query.page) || 1;
+    let skip = page -1;
+    skip = skip * 10;
+
+    const posts = await Post.find()
+    .sort({ _id: -1})
+    .skip(skip)
+    .limit(10)
+    .populate('user', '-password')
+    .exec();
+
+    
+    res.json({
+        ok: true,
+        page,
+        posts
+    })
+
+});
+
+
+// Create post
 postRoutes.post('/', [verifyToken], (req: any, res: Response) => {
 
     const body = req.body;
